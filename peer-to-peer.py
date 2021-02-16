fa2868253cc94043cabd1a34983699e045eb1e59
RuTokken <d.zaitsev91@mail.ru>
2021-02-16 13:16:59 +0300

    final

--- peer-to-peer.py
+++ peer-to-peer.py
@@ -4,13 +4,20 @@ users = [{'name': 'Иван', 'age': 35},
          {'name': 'Мария', 'age': 22},
          {'name': 'Соня', 'age': 20}]
 
-tourists = [{'user': users[0], 'city': cities[2]},
-            {'user': users[1], 'city': cities[0]},
-            {'user': users[2], 'city': cities[1]}]
+tourists = [{'user': users[0], 'city': cities[0]},
+            {'user': users[1], 'city': cities[1]},
+            {'user': users[2], 'city': cities[2]}]
 
 city = input('Введите город: ')
-city == city.lower()
-for i in range (len(city)):
-    city[tourists[i]] = users [i]
-    print('Турист')
 
+if city.upper() == cities[0].upper():
+    print(
+        f"Турист {users[0]['name']} возраст {users[0]['age']} . Посетил город {cities[0]}")
+
+if city.upper() == cities[1].upper():
+    print(
+        f"Турист {users[1]['name']} возраст {users[1]['age']} . Посетил город {cities[1]}")
+
+if city.upper() == cities[2].upper():
+    print(
+        f"Турист {users[2]['name']} возраст {users[2]['age']} . Посетил город {cities[2]}")
